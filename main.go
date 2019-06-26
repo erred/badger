b8e4d0a24bdfbc9dcbc80d805618e113cac7fad6
Sean Liao <seankhliao@gmail.com>
2019-06-26 18:58:40 +0800

    'proper colors'

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 
 const (
 	// ConsoleLink is a link to build history
-	ConsoleLink = "https://console.cloud.google.com/cloud-build/builds"
+	ConsoleLink = "https://console.cloud.google.com/cloud-build/builds?%s"
 	// ShieldsIOLink is a link to the shields image
 	ShieldsIOLink = "https://img.shields.io/badge/BUILD-%s-%s.svg?style=for-the-badge&maxAge=31536000"
 )
@@ -23,27 +23,35 @@ const (
 var (
 	Port       = os.Getenv("PORT")
 	GCPProject = os.Getenv("PROJECT")
-	ColorMap   = map[string]string{
-		"SUCCESS":        "success",
-		"FAILURE":        "warning",
-		"STATUS_UNKNOWN": "informational",
-		"NOT_FOUND":      "inactive",
-		"INTERNAL_ERROR": "critical",
+
+	// Possible values:
+	//   "STATUS_UNKNOWN" - Status of the build is unknown.
+	//   "QUEUED" - Build or step is queued; work has not yet begun.
+	//   "WORKING" - Build or step is being executed.
+	//   "SUCCESS" - Build or step finished successfully.
+	//   "FAILURE" - Build or step failed to complete successfully.
+	//   "INTERNAL_ERROR" - Build or step failed due to an internal cause.
+	//   "TIMEOUT" - Build or step took longer than was allowed.
+	//   "CANCELLED" - Build or step was canceled by a user.
+	Color = map[string]string{
+		"SUCCESS":        "brightgreen",
+		"FAILURE":        "orange",
+		"STATUS_UNKNOWN": "blue",
+		"NOT_FOUND":      "lightgrey",
+		"INTERNAL_ERROR": "red",
 	}
 )
 
-func ShieldLink(status string) string {
-	return fmt.Sprintf(ShieldsIOLink, status, ColorMap[status])
-}
-
-func main() {
+func init() {
 	if Port == "" {
 		Port = ":8080"
 	}
 	if Port[0] != ':' {
 		Port = ":" + Port
 	}
+}
 
+func main() {
 	// Setup / get client
 	client, err := google.DefaultClient(oauth2.NoContext, cloudbuild.CloudPlatformScope)
 	if err != nil {
@@ -57,12 +65,27 @@ func main() {
 	// handle reuquests
 	http.HandleFunc("/i/", func(w http.ResponseWriter, r *http.Request) {
 		repoName := strings.Split(r.URL.Path, "/")[2]
-		s, err := status(svc, GCPProject, repoName)
+		status := "NOT_FOUND"
+		res, err := svc.Projects.Builds.
+			List(GCPProject).
+			Filter(`source.repo_source.repo_name = "` + repoName + `"`).
+			Fields("builds.status").
+			Do()
 		if err != nil {
-			s = "INTERNAL_ERROR"
+			status = "INTERNAL_ERROR"
+			log.Println("get builds status", err)
 		}
-		http.Redirect(w, r, ShieldLink(s), http.StatusFound)
+
+		for _, b := range res.Builds {
+			if b.Status == "WORKING" || b.Status == "QUEUED" || b.Status == "CANCELLED" {
+				continue
+			}
+			status = b.Status
+			break
+		}
+		http.Redirect(w, r, fmt.Sprintf(ShieldsIOLink, status, Color[status]), http.StatusFound)
 	})
+
 	http.HandleFunc("/l/", func(w http.ResponseWriter, r *http.Request) {
 		repoName := strings.Split(r.URL.Path, "/")[2]
 		vals := url.Values{
@@ -73,9 +96,9 @@ func main() {
 				fmt.Sprintf(`source.repo_source.repo_name = "%s"`, repoName),
 			},
 		}
-		u := ConsoleLink + "?" + vals.Encode()
-		http.Redirect(w, r, u, http.StatusFound)
+		http.Redirect(w, r, fmt.Sprintf(ConsoleLink, vals.Encode()), http.StatusFound)
 	})
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
 			http.Redirect(w, r, "/", http.StatusFound)
@@ -83,38 +106,8 @@ func main() {
 		}
 		w.Write(indexHTML)
 	})
+
 	log.Fatal(http.ListenAndServe(Port, nil))
-}
-
-// Possible values:
-//   "STATUS_UNKNOWN" - Status of the build is unknown.
-//   "QUEUED" - Build or step is queued; work has not yet begun.
-//   "WORKING" - Build or step is being executed.
-//   "SUCCESS" - Build or step finished successfully.
-//   "FAILURE" - Build or step failed to complete successfully.
-//   "INTERNAL_ERROR" - Build or step failed due to an internal cause.
-//   "TIMEOUT" - Build or step took longer than was allowed.
-//   "CANCELLED" - Build or step was canceled by a user.
-func status(svc *cloudbuild.Service, p, r string) (string, error) {
-	res, err := svc.Projects.Builds.
-		List(p).
-		Filter(`source.repo_source.repo_name = "` + r + `"`).
-		Fields("builds.status").
-		Do()
-	if err != nil {
-		return "INTERNAL_ERROR", err
-	}
-
-	// filter qorking / queued / cancelled
-	status := "NOT_FOUND"
-	for _, b := range res.Builds {
-		if b.Status == "WORKING" || b.Status == "QUEUED" || b.Status == "CANCELLED" {
-			continue
-		}
-		status = b.Status
-		break
-	}
-	return status, nil
 }
 
 var indexHTML = []byte(`
