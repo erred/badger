a00c297d503c6e38ab4bec04143dbf8744780ebd
Sean Liao <seankhliao@gmail.com>
2019-10-11 15:37:04 +0200

    rewrite

--- go.mod
+++ go.mod
@@ -1,15 +1,17 @@
-module seankhliao.com/badger
+module seankhliao.com/badger/v2
 
 require (
 	cloud.google.com/go v0.46.3 // indirect
+	github.com/golang/groupcache v0.0.0-20191002201903-404acd9df4cc // indirect
 	github.com/google/go-cmp v0.3.1 // indirect
+	github.com/rs/zerolog v1.15.0
 	go.opencensus.io v0.22.1 // indirect
-	golang.org/x/net v0.0.0-20190930134127-c5a3c61f89f3 // indirect
-	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
-	golang.org/x/sys v0.0.0-20191001151750-bb3f8db39f24 // indirect
-	google.golang.org/api v0.10.0
-	google.golang.org/appengine v1.6.4 // indirect
-	google.golang.org/genproto v0.0.0-20190927181202-20e1ac93f88c // indirect
+	golang.org/x/crypto v0.0.0-20191010185427-af544f31c8ac
+	golang.org/x/net v0.0.0-20191009170851-d66e71096ffb // indirect
+	golang.org/x/sys v0.0.0-20191010194322-b09406accb47 // indirect
+	google.golang.org/api v0.11.0
+	google.golang.org/appengine v1.6.5 // indirect
+	google.golang.org/genproto v0.0.0-20191009194640-548a555dbc03 // indirect
 	google.golang.org/grpc v1.24.0 // indirect
 )
 
--- main.go
+++ main.go
@@ -1,186 +1,115 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
 
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
 	"google.golang.org/api/cloudbuild/v1"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/crypto/ssh/terminal"
 )
-
-const (
-	// ConsoleLink is a link to build history
-	ConsoleLink = "https://console.cloud.google.com/cloud-build/builds?%s"
-	// ShieldsIOLink is a link to the shields image
-	ShieldsIOLink = "https://img.shields.io/badge/BUILD-%s-%s.svg?style=flat-square"
-)
-
-var (
-	Port       = os.Getenv("PORT")
-	GCPProject = os.Getenv("PROJECT")
-
-	// Possible values:
-	//   "STATUS_UNKNOWN" - Status of the build is unknown.
-	//   "QUEUED" - Build or step is queued; work has not yet begun.
-	//   "WORKING" - Build or step is being executed.
-	//   "SUCCESS" - Build or step finished successfully.
-	//   "FAILURE" - Build or step failed to complete successfully.
-	//   "INTERNAL_ERROR" - Build or step failed due to an internal cause.
-	//   "TIMEOUT" - Build or step took longer than was allowed.
-	//   "CANCELLED" - Build or step was canceled by a user.
-	Color = map[string]string{
-		"SUCCESS":        "brightgreen",
-		"FAILURE":        "orange",
-		"STATUS_UNKNOWN": "blue",
-		"NOT_FOUND":      "lightgrey",
-		"INTERNAL_ERROR": "red",
-	}
-)
-
-func init() {
-	if Port == "" {
-		Port = ":8080"
-	}
-	if Port[0] != ':' {
-		Port = ":" + Port
-	}
-}
 
 func main() {
-	// Setup / get client
-	client, err := google.DefaultClient(oauth2.NoContext, cloudbuild.CloudPlatformScope)
+	initLog()
+
+	ctx := context.Background()
+	cb, err := cloudbuild.NewService(ctx)
 	if err != nil {
-		log.Fatal("get default client: ", err)
-	}
-	svc, err := cloudbuild.New(client)
-	if err != nil {
-		log.Fatal("get cloudbuild service: ", err)
+		log.Fatal().Err(err).Msg("cloudbuild.NewService")
 	}
 
-	// handle reuquests
-	http.HandleFunc("/i/", func(w http.ResponseWriter, r *http.Request) {
-		repoName := strings.Split(r.URL.Path, "/")[2]
-		status := "NOT_FOUND"
-		res, err := svc.Projects.Builds.
-			List(GCPProject).
-			Filter(`source.repo_source.repo_name = "` + repoName + `"`).
-			Fields("builds.status").
-			Do()
-		if err != nil {
-			status = "INTERNAL_ERROR"
-			log.Println("get builds status", err)
-		}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	project := os.Getenv("PORJECT")
+	if project == "" {
+		log.Fatal().Msg("PROJECT must be set")
+	}
 
-		for _, b := range res.Builds {
-			if b.Status == "WORKING" || b.Status == "QUEUED" || b.Status == "CANCELLED" {
-				continue
-			}
-			status = b.Status
-			break
-		}
-		http.Redirect(w, r, fmt.Sprintf(ShieldsIOLink, status, Color[status]), http.StatusFound)
-	})
-
-	http.HandleFunc("/l/", func(w http.ResponseWriter, r *http.Request) {
-		repoName := strings.Split(r.URL.Path, "/")[2]
-		vals := url.Values{
-			"project": []string{
-				GCPProject,
-			},
-			"query": []string{
-				fmt.Sprintf(`source.repo_source.repo_name = "%s"`, repoName),
-			},
-		}
-		http.Redirect(w, r, fmt.Sprintf(ConsoleLink, vals.Encode()), http.StatusFound)
-	})
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			http.Redirect(w, r, "/", http.StatusFound)
+	http.Handle("/badger/i/", http.StripPrefix("/badger/i/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var tag string
+		if tag = strings.Split(r.URL.Path, "/")[0]; tag == "" {
+			log.Error().Str("url", r.URL.String()).Msg("no tag specified")
+			linkImg(w, r, http.StatusText(http.StatusBadRequest), "red")
 			return
 		}
-		w.Write(indexHTML)
-	})
 
-	log.Fatal(http.ListenAndServe(Port, nil))
+		res, err := cb.Projects.Builds.List(project).Filter(fmt.Sprintf(`tags="%s"`, tag)).Fields("builds.status").Do()
+		if err != nil {
+			log.Error().Str("tag", tag).Err(err).Msg("list builds")
+			linkImg(w, r, http.StatusText(http.StatusInternalServerError), "red")
+			return
+		}
+
+		status, color := "no builds", "red"
+		for _, b := range res.Builds {
+			if b.Status == "CANCELLED" {
+				continue
+			}
+			status = strings.ToLower(b.Status)
+			color = "orange"
+			if status == "success" {
+				color = "brightgreen"
+			}
+			break
+		}
+
+		log.Info().Str("tag", tag).Str("status", status).Msg("served")
+		linkImg(w, r, status, color)
+	})))
+	http.Handle("/badger/i/", http.StripPrefix("/badger/i/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var tag string
+		if tag = strings.Split(r.URL.Path, "/")[0]; tag == "" {
+			log.Error().Str("url", r.URL.String()).Msg("no tag specified")
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+
+		log.Info().Str("tag", tag).Msg("redirected")
+		linkLog(w, r, project, tag)
+	})))
+
+	log.Info().Str("port", port).Msg("serving")
+	http.ListenAndServe(":"+port, nil)
 }
 
-var indexHTML = []byte(`
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
-
-    <title>Badger</title>
-    <meta name="description" content="Markdown badges for Cloud Build results" />
-
-    <style>
-	* {
-	  box-sizing: border-box;
+func initLog() {
+	logfmt := os.Getenv("LOGFMT")
+	if logfmt != "json" {
+		logfmt = "text"
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, NoColor: !terminal.IsTerminal(int(os.Stdout.Fd()))})
 	}
-	body, section {
-	  display: flex;
-	  flex-flow: column nowrap;
-	  align-items: center;
-	  margin: 0;
-	  width: 100%;
+
+	level, _ := zerolog.ParseLevel(os.Getenv("LOGLVL"))
+	if level == zerolog.NoLevel {
+		level = zerolog.InfoLevel
 	}
-	label {
-	  margin-top: 1em;
+	log.Info().Str("FMT", logfmt).Str("LVL", level.String()).Msg("log initialized")
+	zerolog.SetGlobalLevel(level)
+}
+
+func linkImg(w http.ResponseWriter, r *http.Request, status, color string) {
+	u := url.URL{
+		Scheme:   "https:",
+		Host:     "img.shields.io",
+		Path:     "/badge/build-" + status + "-" + color + ".svg",
+		RawQuery: r.URL.RawQuery,
 	}
-	section input {
-	  margin-bottom: 1em;
-	  width: 90%;
+	http.Redirect(w, r, u.String(), http.StatusFound)
+}
+func linkLog(w http.ResponseWriter, r *http.Request, project, tag string) {
+	u := url.URL{
+		Scheme:   "https",
+		Host:     "console.cloud.google.com",
+		Path:     "/cloud-build/builds",
+		RawQuery: url.Values{"project": []string{project}, "query": []string{`tags="` + tag + `"`}}.Encode(),
 	}
-    </style>
-  </head>
-  <body>
-    <h1>Badger</h1>
-    <p>Badges for Cloud Build*</p>
-    <p>
-      <a href="https://github.com/seankhliao/badger">Source on Github</a>
-      <a href="https://badger.seankhliao.com/l/github_seankhliao_badger"
-	<img src="https://badger.seankhliao.com/i/github_seankhliao_badger" />
-      </a>
-    </p>
-    <p>* <em>Only works with projects it had access to</em></p>
-
-    <h2>Generate</h2>
-    <label for"repo">Repo name (github format: <code>github_$user_$repo</code>)</label>
-    <input type="text" id="repo" name="repo"/>
-
-    <section>
-	    <label for"out1">img url: </label>
-	    <input type="text" id="out1" name="out1">
-
-	    <label for"out2">markdown img: </label>
-	    <input type="text" id="out2" name="out2">
-
-	    <label for"out3">markdown with link: </label>
-	    <input type="text" id="out3" name="out3">
-    </section>
-
-
-    <script>
-      const repo = document.querySelector('#repo');
-      const out1 = document.querySelector('#out1');
-      const out2 = document.querySelector('#out2');
-      const out3 = document.querySelector('#out3');
-      const imgLinkUrl = 'https://badger.seankhliao.com/i/';
-      repo.addEventListener('input', function(e){
-	imgUrl = imgLinkUrl + repo.value;
-	linkUrl = 'https://badger.seankhliao.com/l/' + repo.value;
-	out1.value = imgUrl;
-	out2.value = '![Build](' + imgUrl + ')';
-	out3.value = '[![Build](' + imgUrl + ')](' + linkUrl + ')';
-      });
-    </script>
-  </body>
-</html>
-`)
+	http.Redirect(w, r, u.String(), http.StatusFound)
+}
